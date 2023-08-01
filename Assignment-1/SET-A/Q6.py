3c57c672e97ae0c61571140a41c8e0aba61a33d6
TanpureSaiprasad <138004794+TanpureSaiprasad@users.noreply.github.com>
2023-08-01 23:00:50 +0530

    Delete Assignment-1/SET-A directory

--- Assignment-1/SET-A/Q6.py
+++
@@ -1,9 +0,0 @@
-#Q.Write a program to calculate sum of first and last digit of a number. 
-n=int(input("Enter Number:"))
-last=n%10
-first=0
-while(n>0):
-    first=n%10
-    n=n//10
-    
-print("Sum of First & Last digit:",first+last)
